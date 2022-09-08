c9280e09b19500fde58da33d7cf9364e006b11b0
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-08 11:53:08 +0300

    use puts instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@ int main(void)
 
 {
 
-puts(""Programming is like building a multilingual puzzle"\n);
+puts("\"Programming is like building a multilingual puzzle");
 
 return(0);
 
 }
 
-return (main);
+return (void);
