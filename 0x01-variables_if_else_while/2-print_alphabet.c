79e4871cd416f6ba582044bb9b76efc8609a6a3e
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-09 10:37:38 +0300

    print alphabet in lowercase with newline after

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+
+int main(void)
+{
+int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	putchar('\n');
+	getch();
+	}
+
+return (0);
+}
