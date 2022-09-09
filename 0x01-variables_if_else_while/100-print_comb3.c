62921fc0f8861e3f1b8ca63c32e55dc1046dacad
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-09 16:52:06 +0300

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,12 @@ int main(void)
 {
 int a, b;
 
-for (a = 0; a < 9; a++)
+for (a = 0; b <= 9; b++)
 	{
-	for (b = a + 1; b < 10; b++)
+	for (a = a + 1; b <= 9; b++)
 
 	putchar((a % 10) + '0');
 	putchar((b % 10) + '0');
-
-		if (a == 8 && b == 9)
-		continue;
-
 	putchar(',');
 	putchar(' ');
 	}
