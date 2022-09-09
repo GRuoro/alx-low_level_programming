e45b41fa26ce2597821e06834efadbb2122a8eb3
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-09 13:43:25 +0300

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,14 +12,13 @@ int main(void)
 int n;
 char ch;
 
-	for (n = 0; n <= 10 ; n++)
-		{
-		for (ch = 'a'; ch <= 'f'; ch++)
-		putchar(ch);
-		}
-	putchar((n % 16) + '0');
+for (n = 0; n <= 10 ; n++)
+	putchar((n);
 
-	putchar('\n');
+for (ch = 'a'; ch <= 'f'; ch++)
+	putchar(ch);
+		}
+putchar('\n');
 
 return (0);
 }
