53e9fa8035145e04b33869d15ec7330acf84d6ad
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-09 10:43:16 +0300

    just like 2 only in uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+
+int main(void)
+{
+int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	putchar('\n');
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	putchar(ch);
+	getch();
+	}
+
+return (0);
+}
