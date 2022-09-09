8679e487e4e51f4529e5180ae212c242ca376c4f
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-09 11:51:16 +0300

    print numbers base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print all base 16 numbers
+ *
+ * Return: always 0
+ *
+*/
+
+int main(void)
+{
+
+int n;
+int ch;
+
+	for (n ==  0; n <= 10 ; n++)
+	{
+		{
+		for (ch = 'a'; ch <= 'f'; ch++)
+		}
+	putchar(n);
+	putchar(ch);
+	putchar('\n')
+	getch();
+	}
+
+return (0);
+}
