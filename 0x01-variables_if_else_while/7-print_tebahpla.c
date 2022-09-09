3ef5f8d3f1acde0291973c7cbb8da9c4bf83f529
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-09 11:28:42 +0300

    print from z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+		{
+		putchar(ch);
+		putchar('\n');
+		getch();
+		}
+
+	return (0);
+}
