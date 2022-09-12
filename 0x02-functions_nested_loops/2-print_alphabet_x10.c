dca3a191a74424bc9a9e5b9978430d69edb483bf
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-12 22:27:10 +0300

    find function definition in main.h

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,33 +1,4 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-void print_alphabet_x10(void)
-{
-
-char alph = 'a';
-int counter = 0;
-
-for (counter = 0; counter <= 9; counter ++)
-	{
-	for (alph = 'a'; alph <= 'z'; alph++)
-		{
-		_putchar(alph);
-		}
-
-	_putchar('\n');
-	}
-}
+#include "main.h"
 
 /**
  * main - invoke all functions
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include <ctype.h>
+#include "main.h"
+
+int _islower(int c);
+{
+if (islower(c))
+	{
+	return (1);
+	}
+else
+	return (0);
+}
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+_islower();
+
+return (0);
+}
