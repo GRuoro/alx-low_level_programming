bfc1ad8fa7707300a4247bb902a0a1e16f351088
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-13 13:51:00 +0300

    function in file;function call in main

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,19 @@ int print_last_digit(int n)
 
 n = n % 10;
 
-_putchar(n + '0');
-
+if (n < 0)
+	{
+	n = -n;
+	_putchar(n + '0');
+	return(n);
+	}
+else if (n == 0)
+	{
+	_putchar(n + '0');
+	}
+else if (n > 0)
+	{
+	_putchar(n + '0');
+	}
 return (0);
 }
