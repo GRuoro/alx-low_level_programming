6aec2022db6ff88cfde0e1ea2ca7b60ea84f60fa
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-13 11:53:42 +0300

    function in file;function call in main

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ * main - print sign
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+int n;
+
+print_sign(n);
+
+return (0);
+
+}
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,27 @@
 #include "main.h"
 
 /**
- *
- * main - print sign
- *
- * Return: always return 0
+ * print_sign - prints the sign of a number
+ *@n: number being tested
+ * Return: Always 0.
  */
 
-int main(void)
+int print_sign(int n)
 {
-int n;
-
-print_sign(n);
-
-return (0);
-
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
 }
