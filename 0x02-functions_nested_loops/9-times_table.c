a5163b0b386ec15e18ea2a0168312cf87669bc9d
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-14 10:47:53 +0300

    function in file;function call in main

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,8 +15,8 @@ for(i = 0; i <= 9; i++)
 	for(j = 0; j <= 9; j++)
 		{
 		n = i * j;
-		_putchar(n/10);
-		_putchar(n%10 + '0');
+		_putchar(n/100);
+		_putchar(n%100 + '0');
 		_putchar(',');
 		_putchar(' ');
 		}
