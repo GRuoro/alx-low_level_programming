81f920f0f28d7e7ae082f11f5bc0051ea9bbdf46
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-14 12:36:11 +0300

    sum of whole numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - computes and prints the sum of all the multiples of
+ *3 or 5 below 1024 (excluded), followed by a new line
+ *
+*/
+
+int main(void)
+{
+int sum, n;
+
+sum = 0;
+
+for (n = 1; n < 1024; n++)
+	{
+	if ((n % 3 == 0) || (n % 5 == 0))
+		sum = sum + n;
+	}
+printf("Sum = %d\n", sum);
+	
+return (0);
+}
