02ba47ae26a469946b8ad8d6337d77a92f57a05a
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-14 14:03:42 +0300

    98 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,12 +13,12 @@ unsigned long b = 1;
 unsigned long c;
 int i;
 
-for (i = 0; i < 97 ; i++)
+for (i = 0; i < 95 ; i++)
 {
 	c = a + b;
 	printf("%lu", c);
 
-	if (i != 98)
+	if (i != 95)
 		printf(", ");
 	a = b;
 	b = c;
