81c5429b99fa0898aa27c76a5fbecc2741915460
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-14 15:25:41 +0300

    fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -14,15 +14,18 @@ unsigned long c;
 int i;
 
 for (i = 0; i < 49 ; i++)
-{
-
+	{
 	if ((b < 4000000) && (b % 2 == 0))
 		c = a + b;	
+
 	printf("%lu", c);
 	
 	a = b;
 	b = c;
-}
+
+	while (i != 49)
+		printf(", ");
+	}
 printf("\n");
 return (0);
 }
