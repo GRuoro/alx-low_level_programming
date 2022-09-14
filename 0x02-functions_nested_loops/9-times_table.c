cd96716f0432c5f55bb5cf959f31f9a3ae345845
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-14 09:39:18 +0300

    function in file;function call in main

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,15 +8,16 @@
   
 void times_table(void)
 {
-int a, b;
+int a, b, c;
 
 for (a = 0; a <= 9; a++)
 {
         for (b = 0; b <= 9; b++)
         {
-        _putchar('a * b');
+        c = a * b;
+	_putchar(c);
         }
-_putchar(\n);
-
+_putchar('\n');
+}
 return (0);
 }
