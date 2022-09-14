c69aae17eb64d87ce1b262f945bc1267d5f16a89
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-14 14:00:55 +0300

    98 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print fibonacci
+ *
+ *Return: always return 0
+*/
+
+int main(void)
+{
+unsigned long a = 0;
+unsigned long b = 1;
+unsigned long c;
+int i;
+
+for (i = 0; i < 97 ; i++)
+{
+	c = a + b;
+	printf("%lu", c);
+
+	if (i != 98)
+		printf(", ");
+	a = b;
+	b = c;
+}
+printf("\n");
+return (0);
+}
