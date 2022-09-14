afedce62155fb08ecac1d419d59b414f9673fa6e
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-14 14:50:30 +0300

    function in file;function call in main

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,6 +8,47 @@
 
 void times_table(void)
 {
+
+int x = 0; /* factor */
+int y; /* count  */
+int z; /* computed value */
+
+while (x < 10)
+{
+	y = 0;
+	while (y < 10)
+	{
+		z = x * y;
+
+		if (z > 9)
+			{
+			_putchar(z / 10 + '0');
+			_putchar(z % 10 + '0');
+			}
+		else if (y != 0)
+			{
+			_putchar(' ');
+			_putchar(z + '0');
+			}
+		else
+			{
+			_putchar(z + '0');
+			}
+
+		if (y != 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		y++;
+	}
+	_putchar('\n');
+	x++;
+	}
+}
+
+/**void times_table(void)
+{
 int j, i, n;
 
 for (i = 0; i < 10; i++)
@@ -25,4 +66,4 @@ for (i = 0; i < 10; i++)
 		}
 	_putchar('\n');
 	}
-}
+}**/
