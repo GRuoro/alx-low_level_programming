150b579626637238198affd9fb4b7109de417c51
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-15 08:59:10 +0300

    print 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *void print_numbers(void) - prints numbers then newline
+ *
+ *Return:always return 0
+*/
+
+void print_numbers(void)
+{
+int a;
+
+for (a = 0; a <= 9; a++)
+	{
+	_putchar(a);
+	}
+_putchar('\n');
+
+return (0);
+}
