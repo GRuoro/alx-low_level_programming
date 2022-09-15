e540cd2d5788490fd2d8f4c3e7265be0e553b9e0
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-15 10:22:42 +0300

    main update with putchar

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_square - print square of number
+ *@size: size of square
+ *
+*/
+
+void print_square(int size)
+{
+int size;
+int a, b;
+
+while (size > 0)
+	{
+	_putchar('#');
+	size--;
+	}
+}
