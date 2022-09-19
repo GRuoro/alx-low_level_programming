a9226fb92331f175ebb894f97b855e0f938fddaf
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-19 12:10:51 +0300

    Reverse printing

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev - print a string to stdout reverse
+ * print_rev - print a string in reverse
  * @s: char array string type
  */
 
@@ -9,9 +9,11 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] = '\0'; --i)
-	{
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (i--; i >= 0; i--)
 		_putchar(s[i]);
-	}
+
 	_putchar('\n');
 }
