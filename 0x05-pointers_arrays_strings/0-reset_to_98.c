9651271a05c4c7db745a0ee099e26debb6effc91
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-19 10:40:12 +0300

    point to n and change value to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- *reset_to_98 - function hat takes a pointer and updates the value to 98
+ *reset_to_98 - function that takes a pointer and updates the value to 98
  *@n: int parameter
 */
 
 void reset_to_98(int *n)
 {
-int *p;
 
-p = &n;
-
-*p = 98;
+n = &n;
+n = 98;
 
 }
