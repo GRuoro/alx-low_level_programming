f1da7062a35ab153d24f59cfdb5d1b053221e7f0
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-19 11:34:22 +0300

    count string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,6 +3,7 @@
 /**
  *_strlen - returns the length of a string.
  *@s: char parameter
+ *Return: string size
 */
 
 int _strlen(char *s)
@@ -10,5 +11,7 @@ int _strlen(char *s)
 int c;
 
 for (c = 0; s[c] != '\0'; c++)
-	return (c);
+	;
+
+return (c);
 }
