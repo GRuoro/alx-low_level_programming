9c1242b4260e6dd99860171081e181dfafd71f0a
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-19 10:28:06 +0300

    main header file & 1st function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - function hat takes a pointer and updates the value to 98
+ *@n: int parameter
+*/
+
+void reset_to_98(int *n)
+{
+
+int *p = &n;
+
+*p = 98;
+
+}
