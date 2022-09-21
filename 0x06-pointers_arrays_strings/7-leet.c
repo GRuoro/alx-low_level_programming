fc1af4b48d1db909e728be8e1671a5542be4054b
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-21 14:39:28 +0300

    lets get l337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * leet - translates string to 1337
+ * @a: input string to be translated
+ *
+ * Return: a
+ */
+
+char *leet(char *a)
+{
+	int i, j;
+	char key[] = "aAeEoOtTlL";
+	char leet[] = "4433007711";
+
+	for (i = 0; a[i] != '\0'; i++)
+		for (j = 0; j <= 10; j++)
+			if (a[i] == key[j])
+				s[i] = leet[j];
+	return (a);
+}
