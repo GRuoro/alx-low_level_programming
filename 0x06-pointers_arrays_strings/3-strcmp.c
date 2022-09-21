c5acf2ea8fe8ed992e4cb56e551d13ba0c384238
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-21 12:45:12 +0300

    compare strings using pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings
+*@s1: char parameter 1
+*@s2: char parameter 2
+*Return: 0 when '\0' else -1
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 == *s2)
+	{
+	if ((*s1 == '\0') || (*s2 == '\0'))
+		break;
+
+	s1++;
+	s2++;
+	}
+
+if ((*s1 == '\0') && (*s2 == '\0'))
+	return (0);
+else
+	return (-1);
+}
