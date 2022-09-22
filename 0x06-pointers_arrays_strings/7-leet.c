0aa53bf02aa4f38673120685170fc766c2a89f72
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-22 06:25:54 +0300

    lets get 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,7 +14,7 @@ char *leet(char *a)
 	char leet[] = "4433007711";
 
 	for (i = 0; a[i] != '\0'; i++)
-		for (j = 0; j <= 10; j++)
+		for (j = 0; j < 10; j++)
 			if (a[i] == key[j])
 				s[i] = leet[j];
 	return (a);
