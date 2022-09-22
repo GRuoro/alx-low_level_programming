c92266d5e73e20453a536733b8a751b5b6d8c4e6
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-22 06:15:45 +0300

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
 * _strcmp - compares two strings
 *@s1: char parameter 1
 *@s2: char parameter 2
-*Return: 0 when '\0' else -1
+*Return: 0
 */
 
 int _strcmp(char *s1, char *s2)
@@ -21,5 +21,5 @@ while (*s1 == *s2)
 if ((*s1 == '\0') && (*s2 == '\0'))
 	return (s1 - s2);
 else
-	return (-1);
+	return (0);
 }
