6cf22f2f3b133a2dbe1881167eacda8f95567854
GRuoro <111051713+GRuoro@users.noreply.github.com>
2022-09-22 20:06:32 +0300

    print integers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,32 +1,21 @@
 #include "main.h"
 /**
- * print_number - prints # using _putchar function
- * @n: the integer to print
- *
- * Return: void
+ * print_number - print an integer
+ * @n: integer to print
  */
 void print_number(int n)
 {
-int copy, nth, size = 1, ones = n % 10;
-
-	n /= 10;
-	copy = n;
-	if (ones < 0)
+	if (n < 0)
 	{
-		ones *= -1, copy *= -1, n *= -1;
 		_putchar('-');
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
 	}
-	if (copy > 0)
+	else
 	{
-		while (copy / 10 != 0)
-			copy /= 10, size *= 10;
-		while (size > 0)
-		{
-			nth = n / size;
-			_putchar('0' + nth);
-			n -= nth * size;
-			size /= 10;
-		}
+		if (n > 9)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
 	}
-	_putchar('0' + ones);
 }
